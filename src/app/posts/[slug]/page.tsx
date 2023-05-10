fb00fd2623466439e08f121904bb5401b19c4ddf
Clément DOUIN <clement.douin@posteo.net>
2023-05-10 14:45:32 +0200

    add generateStaticParams to posts

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -7,9 +7,18 @@ type Props = {
   params: { slug: string };
 };
 
+export async function generateStaticParams() {
+  const ctx = require.context("../../../posts", false, /\.md$/);
+  const paths = ctx.keys().filter((key) => !key.startsWith("."));
+  return paths
+    .map((path) => path.split("/").pop())
+    .filter((slug) => slug != undefined)
+    .map((slug) => ({ slug: slug!.slice(0, -3) }));
+}
+
 export default async function PostPage({ params: { slug } }: Props) {
-  const module = await import(`../../../posts/${slug}.md`);
-  const { content, data } = matter(module.default);
+  const m = await import(`../../../posts/${slug}.md`);
+  const { content, data } = matter(m.default);
   const { title, date } = data;
   const post: Post = { title, slug, date, content };
 
