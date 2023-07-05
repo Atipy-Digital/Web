91c572c76b67392f51a014d8691ea3eec3d5887c
Damien Bonnet <bonnet.damien@ymail.com>
2023-07-05 16:08:01 +0200

    feat(config): setup linter,ts & github

--- next.config.js
+++ next.config.js
@@ -1,10 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: 'export',
   webpack: (config) => {
     config.module.rules.push({
       test: /\.md$/,
-      use: "raw-loader",
+      use: 'raw-loader',
+    });
+
+    // SVGR
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            typescript: true,
+            icon: true,
+          },
+        },
+      ],
     });
 
     return config;
