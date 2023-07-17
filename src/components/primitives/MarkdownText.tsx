6c0fa4d46e83e71dcc08b0d1750c273e04bf98fa
Damien Bonnet <56789772+Daphaz@users.noreply.github.com>
2023-07-17 14:09:20 +0200

    feat(page): about us page & link from home section (#17)

--- src/components/primitives/MarkdownText.tsx
+++ src/components/primitives/MarkdownText.tsx
@@ -1,15 +1,21 @@
 'use client';
 
 import ReactMarkdown from 'react-markdown';
+import type { SpecialComponents } from 'react-markdown/lib/ast-to-react';
+import type { NormalComponents } from 'react-markdown/lib/complex-types';
 
 import clsxm from '@/lib/clsxm';
 
 export const MarkdownText = ({
   children,
   className = 'text-body1',
+  components,
 }: {
   children: string;
   className?: string;
+  components?: Partial<
+    Omit<NormalComponents, keyof SpecialComponents> & SpecialComponents
+  >;
 }) => {
   return (
     <ReactMarkdown
@@ -24,6 +30,7 @@ export const MarkdownText = ({
             {children}
           </p>
         ),
+        ...components,
       }}
     >
       {children}
---
+++ src/ts/about-page.ts
@@ -0,0 +1,9 @@
+import { CardType } from './card';
+
+export type AboutType = {
+  naissance: string;
+  fusion: string;
+  fusion2: string;
+  challenges: string;
+  cards: CardType[];
+};
