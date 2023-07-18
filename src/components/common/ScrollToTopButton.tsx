7be0f0e789135728ad3e0834b9d17613f94b4945
Damien Bonnet <bonnet.damien@ymail.com>
2023-07-18 17:13:41 +0200

    feat: scroll to top button

---
+++ src/components/common/ScrollToTopButton.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import {
+  motion,
+  useAnimationControls,
+  useScroll,
+  Variants,
+} from 'framer-motion';
+import { useEffect } from 'react';
+
+import { MEDIA_QUERY, useMediaQuery } from '@/hooks/use-media';
+
+import { ATIPY_ICON, AtipyIcon } from './icons/AtipyIcon';
+
+const isBrowser = () => typeof window !== 'undefined';
+function scrollToTop() {
+  if (!isBrowser()) return;
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+const ScrollToTopContainerVariants: Variants = {
+  hide: { opacity: 0 },
+  show: { opacity: 1 },
+};
+
+export const ScrollToTopButton = () => {
+  const { scrollYProgress } = useScroll();
+  const controls = useAnimationControls();
+  const matchXL = useMediaQuery(MEDIA_QUERY.XL);
+  const matchLG = useMediaQuery(MEDIA_QUERY.LG);
+
+  const smallDevice = matchLG ? 0.95 : 0.939;
+  const valueToHide = matchXL ? 0.96 : smallDevice;
+
+  useEffect(() => {
+    return scrollYProgress.on('change', (latestValue) => {
+      if (latestValue > 0.5) {
+        if (latestValue > valueToHide) {
+          controls.start('hide');
+        } else {
+          controls.start('show');
+        }
+      } else {
+        controls.start('hide');
+      }
+    });
+  });
+
+  return (
+    <motion.button
+      className='fixed bottom-0 right-0 p-4 md:p-6 xl:p-8 2xl:p-10 z-[5]'
+      variants={ScrollToTopContainerVariants}
+      initial='hide'
+      animate={controls}
+      onClick={scrollToTop}
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+    >
+      <div className='flex items-center justify-center rounded-full w-10 h-10 lg:w-16 lg:h-16 xl:w-20 xl:h-20 2xl:w-[102px] 2xl:h-[102px]'>
+        <AtipyIcon type={ATIPY_ICON.ARROW_UP} size='full' />
+      </div>
+    </motion.button>
+  );
+};
--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,9 +1,14 @@
+'use client';
+
 import { ReactNode } from 'react';
+
+import { ScrollToTopButton } from '../common/ScrollToTopButton';
 
 export default function Main({ children }: { children: ReactNode }) {
   return (
     <main className='tl relative w-full !mt-[72px] md:!mt-[112px] overflow-x-hidden'>
       {children}
+      <ScrollToTopButton />
     </main>
   );
 }
