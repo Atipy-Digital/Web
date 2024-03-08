374a3294d33bd3ceb7d20da45a110b0b76af96f2
Daviani Fillatre <dfillatre@atipy.fr>
2024-03-08 15:40:57 +0100

    add console.log

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -51,6 +51,7 @@ export async function generateMetadata(
 
 export async function generateStaticParams() {
   const slugs = getPostsSlug();
+  console.log('-------',slugs)
   if (slugs?.length) {
     return slugs.map((slug) => ({ slug }));
   }
