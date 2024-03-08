f1b04127d8395684de3d46541bb0a8f50d3cc9e9
Daviani Fillatre <dfillatre@atipy.fr>
2024-03-08 16:54:24 +0100

    delete console.log

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -51,8 +51,6 @@ export async function generateMetadata(
 
 export async function generateStaticParams() {
   const slugs = getPostsSlug();
-  // eslint-disable-next-line no-console
-  console.log('-------',slugs)
   if (slugs?.length) {
     return slugs.map((slug) => ({ slug }));
   }
