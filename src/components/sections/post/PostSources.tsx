044f4abbcfa22c8ba3b9ccb097d9e04617661c90
Damien Bonnet <bonnet.damien@ymail.com>
2024-03-22 18:41:25 +0100

    fix(post): remove sources if not used

--- src/components/sections/post/PostSources.tsx
+++ src/components/sections/post/PostSources.tsx
@@ -18,22 +18,28 @@ const Link = ({ url, label }: PostSourceLinkType) => (
 );
 
 export const PostSources = ({ source }: Props) => {
+  if (!source?.col1?.length && !source?.col2?.length && !source?.col3?.length) {
+    return null;
+  }
+
   return (
     <Box className='max-w-5xl px-0 md:px-fluid mb-10'>
       <p className='text-body1 py-8 lg:py-12 border-t-2 border-t-current'>
         Sources
       </p>
       <div className='flex flex-col md:flex-row gap-3 lg:gap-10'>
-        <div className='w-full'>
-          {source.col1.map((link) => {
-            if (link?.url && link?.label) {
-              return <Link {...link} key={`post-source-link-${link.url}`} />;
-            }
+        {!!source?.col1?.length && (
+          <div className='w-full'>
+            {source.col1.map((link) => {
+              if (link?.url && link?.label) {
+                return <Link {...link} key={`post-source-link-${link.url}`} />;
+              }
 
-            return null;
-          })}
-        </div>
-        {source.col2 && (
+              return null;
+            })}
+          </div>
+        )}
+        {!!source?.col2?.length && (
           <div className='w-full'>
             {source.col2.map((link) => {
               if (link?.url && link?.label) {
@@ -44,7 +50,7 @@ export const PostSources = ({ source }: Props) => {
             })}
           </div>
         )}
-        {source.col3 && (
+        {!!source?.col3?.length && (
           <div className='w-full'>
             {source.col3.map((link) => {
               if (link?.url && link?.label) {
