45c9aa538bdee2dd86c67f933e4fd9c242698e0b
Daviani Fillatre <dfillatre@atipy.fr>
2024-05-14 12:47:33 +0200

    feat: create mapsite page

---
+++ public/admin/collections/pages/sitemap.js
@@ -0,0 +1,18 @@
+const sitemap = {
+    label: 'Sitemap',
+    name: 'sitemap_page',
+    file: 'src/data/pages/sitemap.md',
+    widget: 'object',
+    editor: {
+        preview: false,
+    },
+    fields: [
+        {
+            label: 'Titre',
+            name: 'title',
+            widget: 'string',
+        },
+    ],
+};
+
+export default sitemap;
---
+++ src/app/sitemap/page.tsx
@@ -0,0 +1,36 @@
+import {Metadata} from 'next';
+
+import {siteOrigin} from '@/lib/constants';
+
+import {HeaderPage} from '@/components/layout/HeaderPage';
+import {Page} from '@/components/layout/Page';
+import dynamic from 'next/dynamic';
+
+
+const SitemapListWithNoSSR = dynamic(() => import('../../components/layout/footer/SitemapList'), {
+    ssr: false,
+});
+
+export const metadata: Metadata = {
+    alternates: {
+        canonical: `${siteOrigin}/sitemap`,
+    },
+    title: 'Plan du site',
+};
+
+export default function Sitemap() {
+
+
+    return (
+        <Page>
+            <HeaderPage
+                title={'Plan du site'}
+                currentLink={{
+                    label: 'Sitemap',
+                    url: 'Sitemap',
+                }}
+            />
+            <SitemapListWithNoSSR />
+        </Page>
+    );
+}
