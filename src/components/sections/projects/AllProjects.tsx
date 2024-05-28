d78a5f59d5cb5639f61fef92dbe1f78bcb2a0ca8
Daviani Fillatre <dfillatre@atipy.fr>
2024-05-28 11:37:40 +0200

    update : création nouveau filtre sur expertise

--- src/components/sections/projects/AllProjects.tsx
+++ src/components/sections/projects/AllProjects.tsx
@@ -1,15 +1,15 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-import { useMemo, useState } from 'react';
+import {useRouter} from 'next/navigation';
 
-import { Box } from '@/components/common/Box';
-import { Button } from '@/components/primitives/Button';
-import { ProjectList } from '@/components/primitives/ProjectList';
+import {useMemo} from 'react';
 
-import { useAppStore } from '@/store/use-app-store';
+import {Box} from '@/components/common/Box';
+import {Button} from '@/components/primitives/Button';
+import {ProjectList} from '@/components/primitives/ProjectList';
 
-import { FilterTags } from './components/FilterTags';
+import {useAppStore} from '@/store/use-app-store';
+import {FilterTagsDropDown} from "@/components/sections/projects/components/FilterTagsDropdown";
 
 export const AllProjects = () => {
   const router = useRouter();
@@ -22,18 +22,23 @@ export const AllProjects = () => {
   const setTagExpertiseActive = useAppStore(
     (store) => store.setTagExpertiseActive
   );
-  const [showAll, setShowAll] = useState<boolean>(false);
 
   const tagsBusinessActive = useMemo(
-    () => businessTags?.filter((t) => t.isActive) ?? [],
+    () => {
+      return businessTags?.filter((t) => t.isActive) ?? [];
+    },
     [businessTags]
   );
   const tagsExpertiseActive = useMemo(
-    () => expertiseTags?.filter((t) => t.isActive) ?? [],
+    () => {
+      return expertiseTags?.filter((t) => t.isActive) ?? [];
+    },
     [expertiseTags]
   );
   const hasTagsActive = useMemo(
-    () => !!tagsBusinessActive.length || !!tagsExpertiseActive.length,
+    () => {
+      return !!tagsBusinessActive.length || !!tagsExpertiseActive.length;
+    },
     [tagsBusinessActive, tagsExpertiseActive]
   );
 
@@ -41,7 +46,7 @@ export const AllProjects = () => {
     if (!projects?.length) return [];
     if (!hasTagsActive) return projects;
 
-    return projects.filter((project) => {
+    const filteredProjects = projects.filter((project) => {
       const hasTagBusiness = project.tags?.some((tag) =>
         tagsExpertiseActive.some(
           (t) => t.label === tag.label && t.color === tag.color
@@ -49,49 +54,38 @@ export const AllProjects = () => {
       );
       const hasTagExpertise = project.client.tags.some((tag) =>
         tagsBusinessActive.some(
-          (t) => t.label === tag.label && t.color === tag.color
+          (t) => t.label === tag.label && t.color === t.color
         )
       );
-
       return hasTagBusiness || hasTagExpertise;
     });
+    return filteredProjects;
   }, [projects, tagsBusinessActive, tagsExpertiseActive, hasTagsActive]);
-
-  const onShowAll = () => {
-    setShowAll((prev) => !prev);
-  };
 
   return (
     <section className='tl mb-14 md:mb-16 lg:mb-20 xl:mb-24'>
-      <Box className='tl px-0 md:px-fluid relative w-full'>
+      <Box className='tl px-0 md:px-fluid relative w-full test'>
         <h3 className='h4 mb-8 md:mb-10 lg:mb-14 w-full text-center font-bold'>
           Toutes nos réalisations
         </h3>
       </Box>
 
       <div className='w-full flex flex-col lg:flex-row justify-between shadow-project-filter px-4 md:px-fluid'>
-        <FilterTags
-          expertiseTags={expertiseTags}
-          businessTags={businessTags}
-          hasTagsBusinessActive={!tagsBusinessActive.length}
-          hasTagsExpertiseActive={!tagsExpertiseActive.length}
-          setTagExpertiseActive={setTagExpertiseActive}
-          setTagBusinessActive={setTagBusinessActive}
-          showAll={showAll}
-        />
-
-        <div className='flex-shrink-0 flex items-center justify-center self-start py-2 lg:pb-0 lg:pt-1 lg:w-44'>
-          <span
-            className='text-grey-110 dark:text-grey-150 underline cursor-pointer text-center lg:px-3'
-            onClick={onShowAll}
-          >
-            {showAll ? 'Réduire les catégories' : 'Afficher plus de catégories'}
-          </span>
+        <div className='mx-auto px-0 md:px-fluid'>
+          <FilterTagsDropDown
+            expertiseTags={expertiseTags}
+            businessTags={businessTags}
+            tagsBusinessActive={tagsBusinessActive}
+            tagsExpertiseActive={tagsExpertiseActive}
+            setTagExpertiseActive={setTagExpertiseActive}
+            setTagBusinessActive={setTagBusinessActive}
+            isFilterExpanded={hasTagsActive}
+          />
         </div>
       </div>
-
-      <Box className='tl px-0 md:px-fluid relative w-full pt-10 md:pt-14 lg:pt-20 xl:pt-24 flex justify-center items-center'>
-        <ProjectList data={projectsFiltered} />
+      <Box
+        className='tl px-0 md:px-fluid relative w-full pt-10 md:pt-14 lg:pt-20 xl:pt-24 flex justify-center items-center'>
+        <ProjectList data={projectsFiltered}/>
       </Box>
 
       <div className='w-full flex items-center justify-center mt-10 lg:mt-14 xl:mt-20'>
--- src/store/use-app-store.tsx
+++ src/store/use-app-store.tsx
@@ -1,4 +1,4 @@
-import { create } from 'zustand';
+import {create} from 'zustand';
 
 import {
   ITagBusiness,
@@ -19,78 +19,100 @@ export interface AppStore {
   setTagsExpertise: (tags: TagExpertiseType[]) => void;
   setTagBusinessActive: (tag: TagBusinessType) => void;
   setTagExpertiseActive: (tag: TagExpertiseType) => void;
+  removeTagBusiness: (tag: TagBusinessType) => void;
+  removeTagExpertise: (tag: TagExpertiseType) => void;
   onResetTags: () => void;
-  onResetTagsExpertise: () => void;
-  onResetTagsBusiness: () => void;
   isOpenModalEngage: boolean;
   setOpenModalEngage: (isOpenModalMenu: boolean) => void;
 }
 
 export const useAppStore = create<AppStore>((set) => ({
   isOpenModalMenu: false,
-  setOpenModalMenu: (isOpenModalMenu: boolean) =>
-    set((s) => ({ ...s, isOpenModalMenu })),
+  setOpenModalMenu: (isOpenModalMenu: boolean) => {
+    set((s) => ({...s, isOpenModalMenu}));
+  },
   isOpenModalEngage: false,
-  setOpenModalEngage: (isOpenModalEngage: boolean) =>
-    set((s) => ({ ...s, isOpenModalEngage })),
+  setOpenModalEngage: (isOpenModalEngage: boolean) => {
+    set((s) => ({...s, isOpenModalEngage}));
+  },
   projects: [],
-  setProjects: (projects: ProjectType[]) => set((s) => ({ ...s, projects })),
+  setProjects: (projects: ProjectType[]) => {
+    set((s) => ({...s, projects}));
+  },
   tagsBusiness: [],
-  setTagsBusiness: (tags: TagBusinessType[]) =>
+  setTagsBusiness: (tags: TagBusinessType[]) => {
     set((s) => ({
       ...s,
-      tagsBusiness: tags.map((t) => ({ ...t, isActive: false })),
-    })),
+      tagsBusiness: tags.map((t) => ({...t, isActive: false})),
+    }));
+  },
   tagsExpertise: [],
-  setTagsExpertise: (tags: TagExpertiseType[]) =>
+  setTagsExpertise: (tags: TagExpertiseType[]) => {
     set((s) => ({
       ...s,
-      tagsExpertise: tags.map((t) => ({ ...t, isActive: false })),
-    })),
-  setTagBusinessActive: (tag: TagBusinessType) =>
+      tagsExpertise: tags.map((t) => ({...t, isActive: false})),
+    }));
+  },
+  setTagBusinessActive: (tag: TagBusinessType) => {
     set((s) => {
       const tagsBusiness = s.tagsBusiness;
       return {
         ...s,
         tagsBusiness: tagsBusiness.map((t) =>
           t.label === tag.label && tag.color === t.color
-            ? { ...t, isActive: !t.isActive }
+            ? {...t, isActive: !t.isActive}
             : t
         ),
       };
-    }),
-  setTagExpertiseActive: (tag: TagExpertiseType) =>
+    });
+  },
+  setTagExpertiseActive: (tag: TagExpertiseType) => {
     set((s) => {
       const tagsExpertise = s.tagsExpertise;
       return {
         ...s,
         tagsExpertise: tagsExpertise.map((t) =>
-          t.label === tag.label && tag.color === t.color
-            ? { ...t, isActive: !t.isActive }
+          t.label === tag.label && t.color === tag.color
+            ? {...t, isActive: !t.isActive}
             : t
         ),
       };
-    }),
-  onResetTagsExpertise: () =>
+    });
+  },
+  removeTagBusiness: (tag: TagBusinessType) => {
+    set((s) => {
+      const tagsBusiness = s.tagsBusiness;
+      return {
+        ...s,
+        tagsBusiness: tagsBusiness.map((t) =>
+          t.label === tag.label && t.color === tag.color
+            ? {...t, isActive: false}
+            : t
+        ),
+      };
+    });
+  },
+  removeTagExpertise: (tag: TagExpertiseType) => {
+    set((s) => {
+      const tagsExpertise = s.tagsExpertise;
+      return {
+        ...s,
+        tagsExpertise: tagsExpertise.map((t) =>
+          t.label === tag.label && t.color === tag.color
+            ? {...t, isActive: false}
+            : t
+        ),
+      };
+    });
+  },
+
+  onResetTags: () => {
     set((s) => {
       return {
         ...s,
-        tagsExpertise: s.tagsExpertise.map((t) => ({ ...t, isActive: false })),
+        tagsExpertise: s.tagsExpertise.map((t) => ({...t, isActive: false})),
+        tagsBusiness: s.tagsBusiness.map((t) => ({...t, isActive: false})),
       };
-    }),
-  onResetTagsBusiness: () =>
-    set((s) => {
-      return {
-        ...s,
-        tagsBusiness: s.tagsBusiness.map((t) => ({ ...t, isActive: false })),
-      };
-    }),
-  onResetTags: () =>
-    set((s) => {
-      return {
-        ...s,
-        tagsExpertise: s.tagsExpertise.map((t) => ({ ...t, isActive: false })),
-        tagsBusiness: s.tagsBusiness.map((t) => ({ ...t, isActive: false })),
-      };
-    }),
+    });
+  },
 }));
