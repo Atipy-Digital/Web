92d376e018fe46862e011e992e173d088771abaf
Daviani <fillatre.daviani@gmail.com>
2024-06-18 16:25:49 +0200

    Devel (#70)
    * chore(devel): release 1.2.0 (#64)
    
    * feat(accessibility): add aria attributes to project collection (#66)
    
    Add 'ariaHidden' and 'ariaLabel' attributes to the project collection settings. These attributes enhance accessibility by providing more information to assistive technologies such as screen readers. The use of these attributes is optional, so they are not marked as required.
    
    Co-authored-by: Daviani Fillatre <dfillatre@atipy.fr>
    
    * chore(devel): release 1.3.0 (#67)
    
    * refactor(filter-tags-dropdown): enhance dropdown menu and tags management (#69)
    
    This commit mainly refactors the FilterTagsDropdown component by revising how expertise and activities dropdown menus are handled, improving their usage of useEffect and useCallback hooks. Additionally, handling of active tags in the DisplayActiveTags component has been optimized by splitting business and expertise tags for better readability.
    
    Co-authored-by: Daviani Fillatre <dfillatre@atipy.fr>
    
    ---------
    
    Co-authored-by: Daviani Fillatre <dfillatre@atipy.fr>

--- src/components/sections/projects/components/DisplayActiveTags.tsx
+++ src/components/sections/projects/components/DisplayActiveTags.tsx
@@ -19,21 +19,27 @@ const DisplayActiveTags = ({
   const removeTagBusiness = useAppStore((s) => s.removeTagBusiness);
   const removeTagExpertise = useAppStore((s) => s.removeTagExpertise);
 
-  // Combine the tags into a single array
-  const allActiveTags = [...activeBusinessTags, ...activeExpertiseTags];
-
   return (
     <div className='flex flex-wrap justify-start gap-2 mt-2 mb-4'>
       <div className='flex flex-wrap gap-2 mb-2'>
-        {allActiveTags.map((tag) => (
+        {activeBusinessTags.map((tag) => (
           <button
             key={tag.label}
             onClick={() => {
-              if ('businessId' in tag) {
-                removeTagBusiness(tag as ITagBusiness);
-              } else {
-                removeTagExpertise(tag as ITagExpertise);
-              }
+              removeTagBusiness(tag);
+            }}
+            className='flex items-center whitespace-nowrap bg-gray-200 text-gray-800 border border-gray-400 px-3 py-1 rounded-full cursor-pointer'
+            aria-label={`Remove ${tag.label}`}
+          >
+            <span className='mr-2 whitespace-nowrap'>{tag.label}</span>
+            <XIcon className='w-5 h-5 text-gray-900' aria-hidden={true} />
+          </button>
+        ))}
+        {activeExpertiseTags.map((tag) => (
+          <button
+            key={tag.label}
+            onClick={() => {
+              removeTagExpertise(tag);
             }}
             className='flex items-center whitespace-nowrap bg-gray-200 text-gray-800 border border-gray-400 px-3 py-1 rounded-full cursor-pointer'
             aria-label={`Remove ${tag.label}`}
